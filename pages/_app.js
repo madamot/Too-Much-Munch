cde0fccdffc661fe9e47296d5978ccdabbcf7b06
Madamot <adam@madamot.com>
2021-03-16 22:49:16 +0000

    redirect to homepage

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ const App = ({ Component, pageProps }) => {
    <Auth0Provider
      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-     redirectUri={url}
+     redirectUri='/'
    >
 
      <Component {...pageProps} />

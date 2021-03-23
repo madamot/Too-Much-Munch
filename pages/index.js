6b9c0461ba2d6c4fb538f44711de7659f8f4b3a1
Madamot <adam@madamot.com>
2021-03-23 21:45:36 +0000

    start to make up the homepage with DatoCMS

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,91 @@ import Head from 'next/head'
 import Layout from '../components/layout/layout'
 import Button from '../components/Button/Button';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css'
+import styled, { css } from 'styled-components';
+// import styles from '../styles/Home.module.css'
 
 import { request } from "../utils/datocms";
 import { StructuredText } from "react-datocms";
 
 import { useAuth0 } from '@auth0/auth0-react';
 
+const Hero = styled.div`
+  font-family: Bebas Neue;
+  padding: 5rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f2f2;
+  color: #0d1c72;
+  width: 100vw;
+  position: relative;
+  margin-left: -50vw;
+  height: auto;
+  left: 50%;
+  padding-top: 5rem;
+}
+`;
+
+const Main = styled.div`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+`;
+
+const Title = styled.h1`
+  line-height: 0.9;
+  text-align: center;
+  font-size: 4.5rem;
+  color: #0d1c72;
+  font-family: Bebas Neue;
+}
+`;
+
+const Munch = styled.span`
+  text-align: center;
+  font-size: 6.4rem;
+  display: block;
+}
+`;
+
+const Subtitle = styled.h5`
+  font-family: Roboto;
+  font-weight: 300;
+  color: #0d1c72;
+  font-size: 2rem;
+}
+`;
+
+const Subsubtitle = styled.h5`
+  font-family: Roboto;
+  font-weight: 100;
+  font-size: 1.5rem;
+  margin: 0;
+}
+`;
+
+
+
 const HOMEPAGE_QUERY = `
   query MyQuery {
     homepage {
       title
+      featuretitle
+      featuresubtitle
     }
   }
 `;
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const data = await request({
     query: HOMEPAGE_QUERY,
-    variables: { limit: 10 }
+    variables: { limit: 10 },
+    preview: context.preview
   });
   return {
     props: { data }
@@ -43,10 +110,15 @@ export default function Home({data}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <Hero>
         {/* <StructuredText data={data.homepage} /> */}
-        <h1>{data.homepage.title}</h1>
-      </main>
+        <Title>Too Much <Munch>Munch</Munch></Title>
+        {/* <Subtitle></Subtitle> */}
+      </Hero>
+      <Main>
+        <Subtitle>{data.homepage.featuretitle}</Subtitle>
+        <Subsubtitle>{data.homepage.featuresubtitle}</Subsubtitle>
+      </Main>
     </Layout>
   )
 }

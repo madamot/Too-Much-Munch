9db25fcf12e01f0960bc77d301bebc2d88f00d83
Madamot <adam@madamot.com>
2021-04-04 21:52:17 +0100

    blog page seo

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -7,6 +7,8 @@ import Button from '../../components/Button/Button';
 import EditForm from '../../components/EditForm/EditForm';
 import { graphQLClient } from '../../utils/graphql-client';
 
+import { useRouter } from 'next/router'
+
 import { StructuredText } from "react-datocms";
 
 import { request } from "../../utils/datocms";
@@ -61,6 +63,14 @@ query MyQuery($id: ItemId) {
           }
         }
       }
+      links {
+        __typename
+        ... on BlogRecord {
+          id
+          slug
+          title
+        }
+      }
     }
   }
 }
@@ -77,12 +87,29 @@ export async function getStaticProps(context) {
   };
 }
 
-export default function BlogPost({ blog }) {
-
+export default function BlogPost({ blog, href }) {
+const router = useRouter()
   console.log(blog);
 
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={blog.blog.subtitle}></meta>
+
+        <meta property="og:title" content={blog.blog.title} key="ogtitle" />
+        <meta property="og:description" content={blog.blog.subtitle} key="ogdesc" />
+
+        <meta property="og:url" content={`www.toomuchmunch.com/blog/${blog.blog.id}`} key="ogurl" />
+        <meta property="og:image" content={blog.blog.blogImage.url} key="ogimage" />
+        <meta property="og:site_name" content='TooMuchMunch' key="ogsitename" />
+        <meta property="og:title" content={blog.blog.title} key="ogtitle" />
+        <meta property="og:description" content={blog.blog.subtitle} key="ogdesc" />
+
+        <title>{blog.blog.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>{blog.blog.title}</h1>
       <h2 style={{
         fontWeight: 200
@@ -92,16 +119,16 @@ export default function BlogPost({ blog }) {
         data={blog.blog.content}
         renderInlineRecord={({ record }) => {
           switch (record.__typename) {
-            case "BlogPostRecord":
-              return <a href={`/blog/${record.slug}`}>{record.title}</a>;
+            case "BlogRecord":
+              return <a target="_blank" href={`/blog/${record.id}`}>{record.title}</a>;
             default:
               return null;
           }
         }}
         renderLinkToRecord={({ record, children }) => {
           switch (record.__typename) {
-            case "BlogPostRecord":
-              return <a href={`/blog/${record.slug}`}>{children}</a>;
+            case "BlogRecord":
+              return <a style={{color: "#848484", borderBottom: ".05em solid #848484", cursor: "pointer"}} target="_blank" href={`/blog/${record.slug}`}>{children}</a>;
             default:
               return null;
           }

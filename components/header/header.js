7f36b56afc476c79bae2ab2dbe22b6cf52d6de0d
Adam Horne <adam@madamot.com>
2021-07-10 23:42:06 +0100

    responsive burger menu

--- components/header/header.js
+++ components/header/header.js
@@ -1,3 +1,4 @@
+import React, { useState, useRef } from "react";
 import styled, { css } from 'styled-components';
 import Button from '../Button/Button';
 import Link from 'next/link';
@@ -65,10 +66,100 @@ const NavItem = styled.li`
 
 `;
 
+const Nav = styled.div`
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+  overflow: auto;
+  align-items: center;
+  justify-content: space-between;
+  width: 100vw;
+
+  @media only screen and (max-width: 40em) {
+      position: fixed;
+      right: 0;
+      bottom: 0;
+      height: 100%;
+      width: 100%;
+      flex-direction: column;
+      z-index: 1;
+      border: none;
+      background-color: #FFF;
+      padding: 2em;
+      padding-top: 70vh;
+      transition: 0.2s ease-out;
+
+      transform: ${({ openDrawer }) =>
+        openDrawer ? `translateX(0)` : `translateX(100%)`};
+    }
+`;
+
+const HamburgerButton = {
+  Wrapper: styled.button`
+    height: 3rem;
+    width: 3rem;
+    position: relative;
+    font-size: 12px;;
+    display: none;
+
+    @media only screen and (max-width: 40em) {
+      display: block;
+    }
+
+    /* Remove default button styles */
+    border: none;
+    background: transparent;
+    outline: none;
+    z-index: 5;
+    cursor: pointer;
+
+    &:after {
+      content: "";
+      display: block;
+      position: absolute;
+      height: 150%;
+      width: 150%;
+      top: -25%;
+      left: -25%;
+    }
+  `,
+  Lines: styled.div`
+    top: 50%;
+    margin-top: -0.125em;
+
+    &,
+    &:after,
+    &:before {
+      /* Create lines */
+      height: 2px;
+      pointer-events: none;
+      display: block;
+      content: "";
+      width: 100%;
+      background-color: black;
+      position: absolute;
+    }
+
+    &:after {
+      /* Move bottom line below center line */
+      top: -0.8rem;
+    }
+
+    &:before {
+      /* Move top line on top of center line */
+      top: 0.8rem;
+    }
+  `
+};
+
 
 const Header = ({dashboard}) => {
   const { isAuthenticated, isLoading, loginWithRedirect, logout } = useAuth0();
   const router = useRouter();
+
+  const [openDrawer, toggleDrawer] = useState(false);
+  const toggleChecked = () => toggleDrawer(value => !value);
+  const drawerRef = useRef(null);
 
   console.log(router.pathname);
   let url = "";
@@ -79,40 +170,47 @@ const Header = ({dashboard}) => {
   return (
     <header>
       <Wrapper>
-        <div>
-          <Link href="/">
-            <a>
-              <Title className="title">🍴 Too Much Munch</Title>
-            </a>
-          </Link>
-        </div>
-        <div>
-          <Link href="/blog">
-            <a>
-              Blog
-            </a>
-          </Link>
-        </div>
-        <div>
-          {!isLoading && (
-            isAuthenticated ? (
-              <div className="multiButtons">
-                <Button size="small" onClick={() => logout({ returnTo: url })} label="Log out" />
-                <Link href='/dashboard'>
-                  <Button primary size="small" label="Dashboard" />
-                </Link>
-              </div>
-            ) : (
-              <div className="multiButtons">
-                <Button size="small" onClick={loginWithRedirect} label="Log in" />
-                <Button primary size="small" onClick={() => loginWithRedirect({
-                  screen_hint: "signup",
-                })} label="Sign Up" />
-              </div>
-            )
-          )}
-        </div>
+        <Nav ref={drawerRef} openDrawer={openDrawer}>
+          <div>
+            <Link href="/">
+              <a>
+                <Title className="title">🍴 Too Much Munch</Title>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="/blog">
+              <a>
+                Blog
+              </a>
+            </Link>
+          </div>
+          <div>
+            {!isLoading && (
+              isAuthenticated ? (
+                <div className="multiButtons">
+                  <Button size="small" onClick={() => logout({ returnTo: url })} label="Log out" />
+                  <Link href='/dashboard'>
+                    <Button primary size="small" label="Dashboard" />
+                  </Link>
+                </div>
+              ) : (
+                <div className="multiButtons">
+                  <Button size="small" onClick={loginWithRedirect} label="Log in" />
+                  <Button primary size="small" onClick={() => loginWithRedirect({
+                    screen_hint: "signup",
+                  })} label="Sign Up" />
+                </div>
+              )
+            )}
+          </div>
+        </Nav>
+        <HamburgerButton.Wrapper onClick={() => toggleChecked(true)}>
+          <HamburgerButton.Lines />
+        </HamburgerButton.Wrapper>
       </Wrapper>
+
+
       {!dashboard ? (
         null
       ) :

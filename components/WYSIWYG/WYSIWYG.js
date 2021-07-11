da23581289faf47df1debe359a2c5fde13cb2648
Adam Horne <adam@madamot.com>
2021-07-11 01:09:30 +0100

    add external links of images into dashboard recipes

--- components/WYSIWYG/WYSIWYG.js
+++ components/WYSIWYG/WYSIWYG.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-import { EditorState, ContentState } from 'draft-js';
+import { EditorState, ContentState, convertToRaw } from 'draft-js';
 import dynamic from 'next/dynamic';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { convertToHTML, convertFromHTML } from 'draft-convert';
 import {stateFromHTML} from 'draft-js-import-html';
@@ -15,17 +17,23 @@ const WYSIWYGEditor = props => {
 
 if (props.convo) {
   useEffect(() => {
-    const blocksFromHTML = convertFromHTML(
-          props.convo
-      );
+    // const blocksFromHTML = convertFromHTML(
+    //       props.convo
+    //   );
 
+      const contentBlock = htmlToDraft(props.convo);
+      if (contentBlock) {
+            const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+            const editorState = EditorState.createWithContent(contentState);
+            setEditorState(editorState);
+          }
 
       // const content = ContentState.createFromBlockArray(
       //     blocksFromHTML.contentBlocks,
       //     blocksFromHTML.entityMap
       // );
 
-      setEditorState(EditorState.createWithContent(blocksFromHTML));
+      // setEditorState(EditorState.createWithContent(blocksFromHTML));
   }, [props.convo]);
 }
 
@@ -42,18 +50,18 @@ if (props.convo) {
   }
 
   const convertContentToHTML = () => {
-    let currentContentAsHTML = convertToHTML({
-      blockToHTML: (block) => {
-        const type = block.type
-        console.log(block);
-        if (type === 'atomic') {
-          let url = block.data.url
-          return { start: "<img src='" + (url) + "'", end: "</img>" }
-        }
-        if (type === 'unstyled') {
-          return <p />
-        }
-      },
+    // let currentContentAsHTML = convertToHTML({
+    //   blockToHTML: (block) => {
+    //     const type = block.type
+    //     if (type === 'atomic') {
+    //       console.log(block.data);
+    //       let url = block.data.src
+    //       return { start: `<img src='${url}'`, end: "</img>" }
+    //     }
+    //     if (type === 'unstyled') {
+    //       return <p />
+    //     }
+    //   },
 
      //  blockToHTML: (block) => {
      //   const type = block.type
@@ -65,7 +73,9 @@ if (props.convo) {
      //     return <p />
      //   }
      // },
-})(editorState.getCurrentContent());
+// })(editorState.getCurrentContent());
+
+let currentContentAsHTML = draftToHtml(convertToRaw(editorState.getCurrentContent()))
     setConvertedContent(currentContentAsHTML);
     console.log("PROPS ==> ", props);
     return props.onChange(
@@ -82,15 +92,22 @@ if (props.convo) {
 }
 
     return (
+      <>
 
-      <Editor
-        toolbar={{
-          options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'emoji', 'remove', 'history'],
-        }}
-        editorState={editorState}
-        onEditorStateChange={handleEditorChange}
-      />
+        <Editor
+          toolbar={{
+            // options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'emoji', 'remove', 'history'],
 
+          }}
+          editorState={editorState}
+          onEditorStateChange={handleEditorChange}
+        />
+
+        <textarea
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        />
+      </>
     );
   }
 
